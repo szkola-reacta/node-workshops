bd7dd0358d84a759f27a1151d66c5ec2a9af08e7
patrykomiotek <patryk.omiotek@gmail.com>
2020-11-16 20:12:24 +0100

    post with body parser

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@
 
 const PORT = 3000;
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 // GET /
 app.get('/', (req, res) => {
   res.send('Hello word!'); // default: 200, text/html
+});
+
+app.post('/', (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  res.json({ ok: true });
 });
 
 app.get('/ping', (req, res) => {
@@ -15,7 +22,14 @@ app.get('/ping', (req, res) => {
   res.status(201).json({ status: 'ok'});
 });
 
-app.listen(PORT);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('Error!');
+    throw err;
+  } else {
+    console.log(`Node server is running on port ${PORT}`);
+  }
+});
 
 // http.createServer(function(request, response) {
 //   const responseData = {
@@ -27,4 +41,3 @@ app.listen(PORT);
 //   response.end(JSON.stringify(responseData));
 // }).listen(PORT);
 
-// console.log(`Node server is running on port ${PORT}`);
