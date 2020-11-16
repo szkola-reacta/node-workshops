0ac432f1f5b2ed999d81c7a37deaa5b72f128988
patrykomiotek <patryk.omiotek@gmail.com>
2020-11-16 20:04:10 +0100

    express intro

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Node workshops
 
+# Install Babel (optional)
+
+Run command:
+
+```bash
+npm install @babel/core @babel/node --save-dev
+```
+
+Install presets
+
+```bash
+npm install @babel/preset-env --save-dev
+```
+
+Create `.babelrc` with:
+
+```json
+{
+  "presets": [
+    "@babel/preset-env"
+  ]
+}
+```
+
+Modify `package.json`:
+
+```json
+"scripts": {
+    "start": "nodemon --exec babel-node src/index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+},
+```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-const http = require('http');
+// const http = require('http');
 
 const PORT = 3000;
+const express = require('express');
 
-http.createServer(function(request, response) {
-  const responseData = {
-    status: 'ok!'
-  };
-  response.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  response.end(JSON.stringify(responseData));
-}).listen(PORT);
+const app = express();
 
-console.log(`Node server is running on port ${PORT}`);
+// GET /
+app.get('/', (req, res) => {
+  res.send('Hello word!'); // default: 200, text/html
+});
+
+app.get('/ping', (req, res) => {
+  // res.json({ status: 'ok'});
+  res.status(201).json({ status: 'ok'});
+});
+
+app.listen(PORT);
+
+// http.createServer(function(request, response) {
+//   const responseData = {
+//     status: 'ok!'
+//   };
+//   response.writeHead(200, {
+//     'Content-Type': 'application/json'
+//   });
+//   response.end(JSON.stringify(responseData));
+// }).listen(PORT);
+
+// console.log(`Node server is running on port ${PORT}`);
